fc4e66f64aa192114f96544660fc48e0cea6f165
Aliakbarshk <aliakbarsk05@gmail.com>
2025-07-28 18:52:19 +0530

    new animations

--- src/components/ffooter.jsx
+++ src/components/ffooter.jsx
@@ -1,32 +1,36 @@
-import React from "react";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-
-
-
 const FFooter = () => {
- 
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      gsap.from("h6", {
+        opacity: 0,
+        y: -150,
+        duration: 1,
+        stagger: 0.1,
+      });
+    },
+    { scope: container }
+  ); // Limit animation to this component only
+
   return (
-    <div className="w-full flex justify-center items-center py-6 bg-gradient-to-r from-[#000000] via-[#000000] to-[#000000] shadow-inner mt-12" >
-      <p className="text-white text-center text-sm flex flex-row gap-2">
-        <h6>N</h6>
-        <h6>O</h6>
-        <h6>T</h6>
-        <h6>H</h6>
-        <h6>I</h6>
-        <h6>N</h6>
-        <h6>G</h6>
-        <h6>H</h6>
-        <h6>E</h6>
-        <h6>R</h6>
-        <h6>E</h6>
-        <h6>🚀</h6>
-      </p>
+    <div
+      ref={container}
+      className="w-full flex justify-center items-center py-6 bg-gradient-to-r from-[#000000] via-[#000000] to-[#000000] shadow-inner mt-12"
+    >
+      <div className="text-white text-center text-sm flex flex-row gap-2">
+        {["N", "O", "T", "H", "I", "N", "G", "H", "E", "R", "E", "🚀"].map(
+          (char, i) => (
+            <h6 key={i}>{char}</h6>
+          )
+        )}
+      </div>
     </div>
   );
-
 };
 
 export default FFooter;
-
